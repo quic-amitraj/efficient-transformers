cfe61b9aa56a037c24690751c2615a2b29c66a14
Amit Raj <amitraj@qti.qualcommm.com>
2025-11-27 08:26:47 +0000

    Code cleanup and fixes
    Signed-off-by: Amit Raj <amitraj@qti.qualcommm.com>

--- QEfficient/__init__.py
+++ QEfficient/__init__.py
@@ -50,8 +50,6 @@ if check_qaic_sdk():
         QEFFCommonLoader,
     )
     from QEfficient.compile.compile_helper import compile
-
-    # Imports for the diffusers
     from QEfficient.diffusers.pipelines.flux.pipeline_flux import QEFFFluxPipeline
     from QEfficient.exporter.export_hf_to_cloud_ai_100 import qualcomm_efficient_converter
     from QEfficient.generation.text_generation_inference import cloud_ai_100_exec_kv
